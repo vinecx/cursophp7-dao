0094ee02bd137a13ae5cb4987db4973de40981fd
vinecx <vinx.alm@gmail.com>
2020-02-19 20:24:45 -0300

    criamos a classe usuario

--- index.php
+++ index.php
@@ -2,10 +2,15 @@
 
 require_once("config.php");
 
-$sql = new Sql();
+////$sql = new Sql();
 
-$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+////$usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
-echo json_encode($usuarios);
+////echo json_encode($usuarios);
 
+$root = new Usuario();
+
+$root->loadbyId(3);
+
+echo $root;
 ?>
