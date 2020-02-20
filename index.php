a1373396c4fa7ad51b9e7806110443448e6daa53
vinecx <vinx.alm@gmail.com>
2020-02-19 21:05:47 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -8,9 +8,31 @@ require_once("config.php");
 
 ////echo json_encode($usuarios);
 
-$root = new Usuario();
+/// CODIGO ABAIXO CARREGA APENAS UM USUARIO.
+/////////$root = new Usuario();
+/////////$root->loadbyId(3);
+/////////echo $root;
+//-----------------------------------------/
+echo "<br>";
+/// CARREGA UMA LISTA DE USUARIOS /
 
-$root->loadbyId(3);
+////$lista = Usuario::getList();
 
-echo $root;
+////echo json_encode($lista);
+
+//----------------------------------------/
+
+/// CARREGA UMA LISTA DE USUARIOS CARREGANDO PELO LOGIN/
+//////////$search = Usuario::search("E");
+//////////echo json_encode($search);
+
+//----------------------------------------/
+
+/// CARREGA UM USUARIO USANDO LOGIN E SENHA
+
+$usuario = new Usuario();
+$usuario->login("jose", "1.5.6.7");
+
+echo $usuario;
+
 ?>
